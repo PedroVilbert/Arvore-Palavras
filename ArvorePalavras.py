3d4a30cf901a9eb31131e9266ec755578c661391
Pedro Vilbert <134733390+PedroVilbert@users.noreply.github.com>
2024-11-18 22:10:44 -0300

    Update ArvorePalavras.py

--- ArvorePalavras.py
+++ ArvorePalavras.py
@@ -11,19 +11,21 @@ dici = [
     'Bola',
     'Bala',
     'Batata',
+    'Casa',
+    'Casamento',
+    'Barco',
+    'Bar',
 ]
 
 tam = 0
 
 for p in dici:
     tam += len(p)
-    
-print(tam) 
-
 
 class No:
     def __init__(self, ch):
         self.ch = ch
+        self.fim = False #marcador de final da palavra
         
         self.filhos = []
         
@@ -41,7 +43,10 @@ class No:
             no = No(ch)
             self.filhos.append(no)
             no.add(palavra[1:])
-    
+        else:
+            #Marcando a ultima letra da palavra como o fim da palavra
+            self.fim = True
+            
     def preordem(self):
         print(self.ch, end =", ")
         
@@ -53,7 +58,7 @@ class No:
         for f in self.filhos:
             if f.ch == ch:
                 if len(palavra) == 1:
-                    return True
+                    return f.fim
                 else:
                     return f.busca(palavra[1:])
                           
@@ -65,5 +70,4 @@ for p in dici:
     raiz.add(p)
 
 raiz.preordem()
-
-print(raiz.busca('banana'))
+print("\n",raiz.busca('bar'))
